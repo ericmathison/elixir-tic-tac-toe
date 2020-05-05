7e5eff55228f1998016b687565fe9de593a1a834
Eric Mathison <dev@ericmathison.io>
2020-05-04 23:59:29 -0700

    Implement Game.move

--- tic_tac_toe.exs
+++ tic_tac_toe.exs
@@ -7,4 +7,15 @@ defmodule Game do
     IO.gets("Select a move (1-9): ")
     |> String.trim()
   end
+
+  def move(board_str, position, player) do
+    cond do
+      position == 0 ->
+        player <> String.slice(board_str, (position + 1)..8)
+      position == 8 ->
+        String.slice(board_str, 0..(position - 1)) <> player
+      true ->
+        String.slice(board_str, 0..(position - 1)) <> player <> String.slice(board_str, (position + 1)..8)
+    end
+  end
 end
