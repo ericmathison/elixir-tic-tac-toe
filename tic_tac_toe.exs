1a771822b30659c1ca38bb6be9d7ba0d21264b22
Eric Mathison <dev@ericmathison.io>
2020-05-13 23:37:04 -0700

    Implement Board.won?

--- tic_tac_toe.exs
+++ tic_tac_toe.exs
@@ -20,4 +20,25 @@ defmodule Game do
         String.slice(board_str, 0..(position - 1)) <> player <> String.slice(board_str, (position + 1)..8)
     end
   end
+
+  def won?(board_str, player) do
+
+    # board positions
+    # 0 1 2
+    # 3 4 5
+    # 6 7 8
+
+    winning_combos = [[0, 3, 6], [1, 4, 7], [2, 5, 8],
+                      [0, 1, 2], [3, 4, 5], [6, 7, 8],
+                      [0, 4, 8], [6, 4, 2]]
+    Enum.any?(Enum.map(winning_combos, fn combo ->
+      if String.at(board_str, Enum.at(combo, 0)) == player and
+         String.at(board_str, Enum.at(combo, 1)) == player and
+         String.at(board_str, Enum.at(combo, 2)) == player do
+        true
+      else
+        false
+      end
+    end))
+  end
 end
